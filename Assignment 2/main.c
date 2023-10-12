56a1ce7b9046fd6ef57a94a14815cbaecfb1cd84
Riley M <rileymeyerkorth@gmail.com>
2023-10-12 12:39:35 -0500

    Assignment 2 init

---
+++ Assignment 2/main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int userInput = 2;
+
+    while (userInput > 1)
+    {
+        printf("Enter 0 or 1 to STOP.\n");
+        printf("Enter the NFL score: ");
+        scanf("%d", &userInput);
+
+        printf("Possible combinations of scoring plays: ");
+        
+    }
+    
+
+    return 0;
+}
