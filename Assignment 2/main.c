ef1494fe89d295a5bc4d0f741b7974ae8e4075a0
Riley M <rileymeyerkorth@gmail.com>
2023-10-12 20:43:31 -0500

    completed assignment 2

--- Assignment 2/main.c
+++ Assignment 2/main.c
@@ -1,29 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define TD 6
-#define FG 3
 #define S 2
-#define TD2P 8
+#define FG 3
+#define TD 6
 #define TDFG 7
+#define TD2P 8
+
+void nfl_score_combinations(int target_score) {
+    // Define the scoring plays and their values
+    char *scoring_plays[] = {"TD + 2pt", "TD + FG", "TD", "3pt FG", "Safety"};
+    int values[] = {8, 7, 6, 3, 2};
+
+    // Iterate through all possible combinations
+    for (int td_2pt = 0; td_2pt <= target_score / values[0]; td_2pt++) {
+        for (int td_fg = 0; td_fg <= target_score / values[1]; td_fg++) {
+            for (int td = 0; td <= target_score / values[2]; td++) {
+                for (int fg = 0; fg <= target_score / values[3]; fg++) {
+                    for (int safety = 0; safety <= target_score / values[4]; safety++) {
+                        if (td_2pt * values[0] + td_fg * values[1] + td * values[2] + fg * values[3] + safety * values[4] == target_score) {
+                            printf("%d %s, %d %s, %d %s, %d %s, %d %s\n", td_2pt, scoring_plays[0], td_fg, scoring_plays[1], td, scoring_plays[2], fg, scoring_plays[3], safety, scoring_plays[4]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
     //-- VARIABLE DECLARATIONS --//
-    int userInput = 2;
+    int score = 2;
     
     //-- MAIN LOOP --//
     printf("Enter 0 or 1 to STOP.\n");
-    while (userInput > 1)
+    while (score > 1)
     {
         printf("Enter the NFL score: ");
-        scanf("%d", &userInput);
 
-        //TODO - Bruteforce Functionality
+        // Get user input
+        scanf("%d", &score);
 
-        printf("Possible combinations of scoring plays: ");
+        printf("Possible combinations of scoring plays: \n");
+        nfl_score_combinations(score);
+        printf("\n");
+
     }
-    
 
     return 0;
 }
